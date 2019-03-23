c67172d0ca45f66d157cbe772d81a17fa44edf91
Anup Sukumaran <anup.sukumaran9@gmail.com>
2019-03-23 20:45:52 +0530

    Initial Commit

---
+++ LogginTut/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LogginTut
+//
+//  Created by Sukumar Anup Sukumaran on 23/03/19.
+//  Copyright © 2019 Qaptive_Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
